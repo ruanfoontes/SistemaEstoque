107028aca340b554e04d53ca03f0710d59fd6e9e
Ruan fontes <ruanfontes309@gmail.com>
2026-03-24 23:25:17 -0300

    feat: dia Finalizado 24/03/2026 ás 23:25

--- target/classes/static/javascript/script.js
+++ target/classes/static/javascript/script.js
@@ -91,7 +91,7 @@ function getFormData() {
 async function submitToServer() {
     // Adicionamos a URL completa do seu servidor Java
     console.log("Tentando enviar para o Java...");
-    const baseUrl = 'http://localhost:8080/'; 
+    const baseUrl = 'http://localhost:8080';
     const path = form.dataset.endpoint || '/register';
     const endpoint = baseUrl + path;
 
