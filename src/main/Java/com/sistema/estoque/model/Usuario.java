b4bda6ab671d2def523fbce7e8011f1420f7a6ac
Ruan fontes <ruanfontes309@gmail.com>
2026-04-03 23:56:26 -0300

    feat:Criado pasta para integração do Banco de Dados e organizado POM.XMl para o Banco de Dados

---
+++ src/main/Java/com/sistema/estoque/model/Usuario.java
@@ -0,0 +1,29 @@
+package com.sistema.estoque.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    public Usuario() {}
+
+    public Long getId() {return id; }
+    public String getNome() {return nome; }
+    public void setNome(String nome) {this.nome = nome; }
+    public String getEmail() {return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+}
